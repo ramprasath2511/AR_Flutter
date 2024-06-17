6d93f5d759b6c70b1de6e8f139456abb159f35e6
Ramprasath <ramprasath@deliveryapp.com>
2024-06-17 14:06:01 +0100

    AR plugin implemented

---
+++ lib/localAndWebObjectsView.dart
@@ -0,0 +1,179 @@
+
+import 'package:ar_flutter_plugin_flutterflow/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin_flutterflow/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin_flutterflow/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin_flutterflow/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin_flutterflow/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin_flutterflow/models/ar_node.dart';
+import 'package:ar_flutter_plugin_flutterflow/widgets/ar_view.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+class LocalAndWebObjectsView extends StatefulWidget {
+  const LocalAndWebObjectsView({Key? key}) : super(key: key);
+
+  @override
+  State<LocalAndWebObjectsView> createState() => _LocalAndWebObjectsViewState();
+}
+
+class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
+  late ARSessionManager arSessionManager;
+  late ARObjectManager arObjectManager;
+
+  //String localObjectReference;
+  ARNode? localObjectNode;
+
+  //String webObjectReference;
+  ARNode? webObjectNode;
+
+  @override
+  void dispose() {
+    arSessionManager.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkCameraPermission();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Local / Web Objects"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(22),
+                child: ARView(
+                  onARViewCreated: onARViewCreated,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: onLocalObjectButtonPressed,
+                      child: const Text("Add / Remove Local Object")),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: onWebObjectAtButtonPressed,
+                      child: const Text("Add / Remove Web Object")),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
+    this.arSessionManager = arSessionManager;
+    this.arObjectManager = arObjectManager;
+    _checkCameraPermissionAndInitialize(arSessionManager, arObjectManager);
+  }
+
+  Future<void> _checkCameraPermissionAndInitialize(
+      ARSessionManager arSessionManager, ARObjectManager arObjectManager) async {
+    var status = await Permission.camera.status;
+    if (status.isGranted || await Permission.camera.request().isGranted) {
+      // Permission granted, initialize AR session
+      this.arSessionManager.onInitialize(
+        showFeaturePoints: false,
+        showPlanes: true,
+        customPlaneTexturePath: "assets/triangle.png",
+        showWorldOrigin: true,
+        handleTaps: false,
+      );
+      this.arObjectManager.onInitialize();
+    } else {
+      // Permission denied, handle accordingly
+      _showPermissionDeniedDialog();
+    }
+  }
+
+  Future<void> _checkCameraPermission() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      if (await Permission.camera.request().isGranted) {
+        // Permission granted, proceed with AR session initialization
+      } else {
+        // Permission denied, handle accordingly
+        _showPermissionDeniedDialog();
+      }
+    } else {
+      // Permission already granted, proceed with AR session initialization
+    }
+  }
+
+  void _showPermissionDeniedDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Camera Permission Denied'),
+          content: Text('The app requires camera access to function properly. Please grant camera permission in the settings.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> onLocalObjectButtonPressed() async {
+    if (localObjectNode != null) {
+      arObjectManager.removeNode(localObjectNode!);
+      localObjectNode = null;
+    } else {
+      var newNode = ARNode(
+          type: NodeType.localGLTF2,
+          uri: "assets/Chicken_01/Chicken_01.gltf",
+          scale: Vector3(0.2, 0.2, 0.2),
+          position: Vector3(0.0, 0.0, 0.0),
+          rotation: Vector4(1.0, 0.0, 0.0, 0.0));
+      bool? didAddLocalNode = await arObjectManager.addNode(newNode);
+      localObjectNode = (didAddLocalNode!) ? newNode : null;
+    }
+  }
+
+  Future<void> onWebObjectAtButtonPressed() async {
+    if (webObjectNode != null) {
+      arObjectManager.removeNode(webObjectNode!);
+      webObjectNode = null;
+    } else {
+      var newNode = ARNode(
+          type: NodeType.webGLB,
+          uri:
+              "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Fox/glTF-Binary/Fox.glb",
+          scale: Vector3(0.2, 0.2, 0.2));
+      bool? didAddWebNode = await arObjectManager.addNode(newNode);
+      webObjectNode = (didAddWebNode!) ? newNode : null;
+    }
+  }
+}
